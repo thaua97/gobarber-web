5a4a04216848d5b5abc4112de40ac25d3bafe58f
Thauã Borges <thaua97@gmail.com>
2019-08-06 15:50:38 -0300

    Adiciona Toastify

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,24 +1,30 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFail } from './actions';
 
 export function* signIn({ payload }) {
-    const { email, password } = payload;
+    try {
+        const { email, password } = payload;
 
-    const res = yield call(api.post, 'sessions', { email, password });
+        const res = yield call(api.post, 'sessions', { email, password });
 
-    const { token, user } = res.data;
+        const { token, user } = res.data;
 
-    if (!user.provider) {
-        console.tron.error('Usuario não é prestador');
+        if (!user.provider) {
+            toast.error('Usuario não é prestador');
+        }
+
+        yield put(signInSuccess(token, user));
+
+        history.push('/dashboard');
+    } catch (err) {
+        toast.error('Falha na autenticação, verifique seus dados');
+        yield put(signFail());
     }
-
-    yield put(signInSuccess(token, user));
-
-    history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
