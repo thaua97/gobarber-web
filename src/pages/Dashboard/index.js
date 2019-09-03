45c83b4226527df2307e5cb8494118ca2d80b7a6
Thauã Borges <thaua97@gmail.com>
2019-09-03 11:55:42 -0300

    edita dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { format, subDays, addDays } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
@@ -6,10 +6,22 @@ import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
 function Dashboard() {
     const [date, setDate] = useState(new Date());
 
     const dateFormatt = useMemo(() => format(date, "d 'de' MMMM", { locale: pt }), [date]);
+
+    useEffect(() => {
+        async function loadSchedule() {
+            const res = await api.get('schedule', {
+                params: { date },
+            });
+
+            const data = range.map(hour => {});
+        }
+    }, [date]);
 
     function handlePrevDay() {
         setDate(subDays(date, 1));
